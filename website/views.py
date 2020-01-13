56412c5c3fee2198e34b199292fd8580694c4cd8
John Elder <john@codemy.com>
2020-01-13 11:29:15 -0800

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request, 'home.html', {})
+
+def contact(request):
+	if request.method == "POST":
+		#do stuff
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		# Send email
+		send_mail(
+			message_name, #subject
+			message, # message
+			message_email, # from emai;
+			['john@codemy.com'], # to email
+			fail_silently=False,
+			)
+
+
+		return render(request, 'contact.html', {'message_name': message_name})
+
+	else:
+		# return the page
+		return render(request, 'contact.html', {})
