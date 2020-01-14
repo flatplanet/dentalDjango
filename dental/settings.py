45231234d48465c2c5d16e9431ad123d4d15bef6
John Elder <john@codemy.com>
2020-01-14 09:45:39 -0800

    tweaked app for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -1,4 +1,9 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dental.urls'
@@ -112,6 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 '''
 EMAIL_HOST = 'smtp.gmail.com'
@@ -122,6 +129,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 '''
 
+# python -m smtpd -n -c DebuggingServer localhost:1025
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 EMAIL_HOST_USER = ''
@@ -129,3 +137,4 @@ EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 
 
+django_heroku.settings(locals())
