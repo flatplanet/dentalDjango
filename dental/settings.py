4f39618ae285e615f24ac739a7d5e28e4ce28f7d
John Elder <john@codemy.com>
2020-01-16 11:32:17 -0800

    commented out sendmail server

--- dental/settings.py
+++ dental/settings.py
@@ -128,13 +128,13 @@ EMAIL_HOST_PASSWORD = 'drPassword'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 '''
-
+'''
 # python -m smtpd -n -c DebuggingServer localhost:1025
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
-
+'''
 
 django_heroku.settings(locals())
